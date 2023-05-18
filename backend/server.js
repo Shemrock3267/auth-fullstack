345ec795e475799d1167a0c883eefd0ef6aa8d0f
Shemrock3267 <dev.oliinyk@gmail.com>
2023-05-18 19:49:55 +0300

    modified registration model controller and encryption

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,11 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/users', userRoutes);
-app.get('/',(req,res) => res.send('Server is ready'));
+
+app.get('/', (req, res) => res.send('Server is ready'));
 
 app.use(notFound);
 app.use(errorHandler);
