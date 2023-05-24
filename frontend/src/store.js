62273bf9f41bb89a4b68a3d96aaa354bb3f5edf1
Shemrock3267 <dev.oliinyk@gmail.com>
2023-05-24 17:28:50 +0300

    added usersAoiSlice with login POST method

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import { apiSlice } from './slices/apiSlice';
 
 const store = configureStore({
-  reducer: { auth: authReducer },
-  middleware: (gDM) => gDM(),
+  reducer: { auth: authReducer, [apiSlice.reducerPath]: apiSlice.reducer },
+  middleware: (gDM) => gDM().concat(apiSlice.middleware),
   devTools: true,
 });
 
